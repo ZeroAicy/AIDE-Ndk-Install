85a281f6d94a49644632274cd8955dc41a51d1d5
ZeroAicy <ZeroAicy@qq.com>
2024-12-07 23:33:38 +0800

    v2.1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,417 @@
-import java.util.*;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
+import android.content.Context;
+import android.os.Build;
+import android.system.ErrnoException;
+import io.github.zeroaicy.util.ContextUtil;
+import io.github.zeroaicy.util.FileUtil;
+import io.github.zeroaicy.util.IOUtils;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.io.FileInputStream;
+import android.text.TextUtils;
 
-		Scanner input = new Scanner(System.in);
+public class Main {
 
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
 
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
 
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+	public static File getAideNdkInstallDir( Context context ) {
+		return new File(getNoBackupFilesDir(context), getNdkSupportVersionFileName());
 	}
+
+	public static final File getNoBackupFilesDir( Context context ) {
+        if ( Build.VERSION.SDK_INT >= 21 ) {
+            return context.getNoBackupFilesDir();
+        }
+        return createDir(new File(context.getApplicationInfo().dataDir, "no_backup"));
+    }
+
+	private static synchronized File createDir( File file ) {
+		if ( file.exists() || file.mkdirs() ) {
+			return file;
+		}
+		if ( file.exists() ) {
+			return file;
+		}
+		return null;
+	}
+
+	public static String getNdkSupportVersionFileName( ) {
+		return "ndksupport-" + getNdkVersion();
+	}
+
+	private static int getNdkVersion( ) {
+		int ndkVersion = Build.CPU_ABI.contains("x86") ? 0x65f03102 : 0x65f03101;
+		return Build.VERSION.SDK_INT >= 26 ? ndkVersion + 2 : ndkVersion;
+    }
+
+	public static final String busyboxResourceName = "/data/busybox";
+	public static final String ndkInstallShellResourceName = "/data/ndk-install.sh";
+	
+	// Ndk 版本名称 例如 r24 r27b
+	// $NDK_VERSION_NAME环境变量
+	static String NDK_VERSION_NAME; // 勿动
+	// Ndk 版本代码 例如 24.0.8215888 27.1.12297006
+	// $NDK_VERSION_CODE环境变量
+	static String NDK_VERSION_CODE = "27.1.12297006"; // 勿动
+
+
+	// 
+	// Ndk包路径 需要特定的Ndk.zip包
+	// Zip 根目录为 android-ndk-xxx 例如 android-ndk-r24 android-ndk-r27b
+	// 唯一主要修改的
+	static String NdkZipFilePath = "/storage/emulated/0/.MyAicy/源码备份/AIDE+/AIDE+Ndk/android-sdk/ndk/android-ndk-r27b-aarch64.zip";
+
+
+	public static void main( String[] args ) throws Exception {
+
+
+		File ndkZipFile = new File(NdkZipFilePath);
+		if ( !ndkZipFile.exists() ) {
+			System.out.println("NdkZip文件不存,在请更改NdkZipFilePath变量");
+			return;
+		}
+
+		Context context = ContextUtil.getContext();
+
+		File filesDir = context.getFilesDir();
+
+		// busybox 安装目录
+        final File busyboxInstallDir;
+		// $HOME变量
+        final File homeDir;
+
+        // 兼容AIDE Pro
+        if ( "aidepro.top".equals(context.getPackageName()) ) {
+            homeDir = new File(filesDir, "framework");
+            busyboxInstallDir = new File(filesDir, "usr/bin/applets");
+        } else {
+            homeDir = new File(filesDir, "home");
+            busyboxInstallDir = new File(filesDir + "/usr/bin");
+        }
+
+        if ( homeDir.isFile() ) {
+            homeDir.delete();
+        }
+        if ( !homeDir.exists() ) {
+            homeDir.mkdirs();
+        }
+        // busybox 安装路径
+
+        if ( busyboxInstallDir.isFile() ) {
+            busyboxInstallDir.delete();
+        } else {
+			// 删除 busyboxInstallDir目录
+			FileUtil.deleteFolder(busyboxInstallDir);
+		}
+		// 创建文件夹
+        if ( !busyboxInstallDir.exists() ) {
+            busyboxInstallDir.mkdirs();
+        }
+
+		String homeDirPath = homeDir.getAbsolutePath();
+
+
+		// 计算 NDK_VERSION_CODE 与 NDK_VERSION_NAME
+		System.out.println("解析Ndk版本信息...");		
+		ZipFile zipFile = new ZipFile(ndkZipFile);
+		Enumeration<? extends ZipEntry> entries = zipFile.entries();
+		while ( entries.hasMoreElements() ) {
+			ZipEntry zipEntry = entries.nextElement();
+			String name = zipEntry.getName();
+			int rootZipEntryNameEnd = name.indexOf('/');
+			
+			if ( rootZipEntryNameEnd > 0 ) {
+				name = name.substring(0, rootZipEntryNameEnd);
+			}
+			if ( name.startsWith("android-ndk-") 
+				&& !name.contains("/") ) {
+				Main.NDK_VERSION_NAME = name.substring("android-ndk-".length());
+				System.out.printf("NDK_VERSION_NAME = %s\n", NDK_VERSION_NAME);
+				break;
+			}
+			System.out.println(name);
+		}
+		ZipEntry sourceProperties = zipFile.getEntry("android-ndk-" + Main.NDK_VERSION_NAME + "/source.properties");
+		if ( sourceProperties != null ) {
+			for ( String line :	IOUtils.readLines(zipFile.getInputStream(sourceProperties)) ) {
+				if ( line.startsWith("Pkg.Revision = ") ) {
+					NDK_VERSION_CODE = line.substring("Pkg.Revision = ".length());
+					System.out.printf("NDK_VERSION_CODE = %s\n", Main.NDK_VERSION_CODE);
+					break;
+				}
+			}
+		}
+		zipFile.close();
+
+		if ( TextUtils.isEmpty(Main.NDK_VERSION_NAME) ) {
+			System.out.println("未能解析出Ndk版本信息 -> Main.NDK_VERSION_NAME");
+			System.out.println("请使用符合的 Ndk包");
+			return;
+		}
+
+
+		// 解压busybox
+		System.out.println("写入并安装busybox...");
+		writeBusybox(busyboxInstallDir);
+
+		// 安装脚本路径
+		System.out.println("写入ndk-install.sh...");
+		File ndkInstallFile = new File(homeDir, "ndk-install.sh");
+		writeNdkInstallFile(ndkInstallFile);
+
+		// 写入cmake
+		System.out.println("写入cmake...");
+		writeResource("android-sdk/cmake/cmake-3.10.2-android-aarch64.zip", new File(homeDirPath, "android-sdk/cmake/cmake-3.10.2-android-aarch64.zip"));		
+		writeResource("android-sdk/cmake/cmake-3.18.1-android-aarch64.zip", new File(homeDirPath, "android-sdk/cmake/cmake-3.18.1-android-aarch64.zip"));		
+		writeResource("android-sdk/cmake/cmake-3.22.1-android-aarch64.zip", new File(homeDirPath, "android-sdk/cmake/cmake-3.22.1-android-aarch64.zip"));		
+		writeResource("android-sdk/cmake/cmake-3.25.1-android-aarch64.zip", new File(homeDirPath, "android-sdk/cmake/cmake-3.25.1-android-aarch64.zip"));		
+
+
+		// 复制Ndk
+		System.out.println("复制Ndk到安装缓存区...");		
+		FileInputStream ndkZipFileInputStream = new FileInputStream(ndkZipFile);
+		FileOutputStream ndkZipFileOutputStream = new FileOutputStream(new File(homeDir, String.format("android-ndk-%s-aarch64.zip", NDK_VERSION_NAME)));
+		IOUtils.streamTransfer(ndkZipFileInputStream, ndkZipFileOutputStream, true);
+
+
+		// 运行脚本 
+		runNdkInstallShellFile(homeDirPath, busyboxInstallDir, ndkInstallFile);  
+		
+		// 添加运行权限
+		
+		System.out.println("添加执行权限...");		
+		Runtime.getRuntime().exec("chmod -R 777 " + homeDirPath);
+
+		// link_aide_ndk
+		// 使用 ln 链接 Ndk 可以复用gradle ndk
+
+		try {
+
+			// AIDE Ndk安装路径
+			File aideNdkInstallDir = getAideNdkInstallDir(context);
+			aideNdkInstallDir.mkdirs();
+
+
+			File installedFile = new File(aideNdkInstallDir, ".installed");
+			// 重置AIDE NDK 安装完成标志
+			installedFile.delete();
+
+			System.out.println("以软链接方式[类似Windows的快捷方式] 安装AIDE NDK");
+
+			String busyboxInstallDirPath = busyboxInstallDir.getAbsolutePath();
+
+			File binDir = new File(aideNdkInstallDir, "bin");
+			if ( binDir.exists() ) {
+				// 软链接 不能递归删除
+				if ( Files.isSymbolicLink(binDir.toPath()) ) {
+					binDir.delete();
+				} else {
+					// 因为是目录所以没删掉😓
+					FileUtil.deleteFolder(binDir);
+				}
+			}
+
+			String binDirPath = binDir.getAbsolutePath();
+			// symlink时必须保证 newPath不存在 
+			android.system.Os.symlink(busyboxInstallDirPath, binDirPath);
+
+
+			File android_ndk_aide_dir = new File(aideNdkInstallDir, "android-ndk-aide");
+			if ( binDir.exists() ) {
+				// android_ndk_aide_dir也是是目录
+				// 软链接 不能递归删除
+				if ( Files.isSymbolicLink(android_ndk_aide_dir.toPath()) ) {
+					android_ndk_aide_dir.delete();
+				} else {
+					FileUtil.deleteFolder(android_ndk_aide_dir);
+				}
+			}
+			android.system.Os.symlink(homeDirPath + "/android-sdk/ndk/" + NDK_VERSION_CODE, android_ndk_aide_dir.getAbsolutePath());
+			installedFile.createNewFile();
+		}
+		catch (ErrnoException e) {
+			System.out.println("链接失败");
+			e.printStackTrace();
+
+			return;
+		}
+
+		System.out.println("安装结束");
+	}
+
+	private static void runNdkInstallShellFile( String homeDirPath, File busyboxInstallDir, File ndkInstallFile ) {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		Map<String, String> env = processBuilder.environment();
+		env.put("HOME", homeDirPath);
+		env.put("PATH", busyboxInstallDir.getAbsolutePath() + ":" + env.get("PATH"));
+		env.put("NDK_VERSION_NAME", Main.NDK_VERSION_NAME);
+		env.put("NDK_VERSION_CODE", Main.NDK_VERSION_CODE);
+
+		List<String> argsList = new ArrayList<>();
+
+		argsList.add(ndkInstallFile.getAbsolutePath());
+		processBuilder.command(argsList);
+		processBuilder.redirectErrorStream(true);
+
+		try {  
+            // 启动子进程  
+            Process process = processBuilder.start();  
+
+            // 读取合并后的输出流  
+            try {
+				InputStream inputStream = process.getInputStream();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;  
+                while ( ( line = reader.readLine() ) != null ) {  
+                    // 处理每一行输出  
+                    System.out.println(line);  
+                }  
+
+                // 等待子进程结束  
+                int exitCode = process.waitFor();  
+                System.out.println("Exited with code: " + exitCode);
+				
+				reader.close();
+				inputStream.close();
+            }
+			catch (Throwable e) {
+				e.printStackTrace();
+			}
+        }
+		catch (IOException e) {  
+            // 处理异常  
+            e.printStackTrace();  
+        }
+	}
+
+	private static void writeNdkInstallFile( File ndkInstallFile ) throws IOException {
+		writeResource(Main.ndkInstallShellResourceName, ndkInstallFile);		
+		ndkInstallFile.setExecutable(true);
+	}
+
+
+	private static void writeBusybox( File busyboxInstallDir ) throws IOException {
+		FileUtil.deleteFolder(busyboxInstallDir);
+		busyboxInstallDir.mkdirs();
+
+		File busyboxFile = new File(busyboxInstallDir, "busybox");
+		writeResource(Main.busyboxResourceName, busyboxFile);		
+		
+		busyboxFile.setExecutable(true);
+		
+		installBusybox(busyboxInstallDir, busyboxFile);
+	}
+
+	private static void installBusybox( File busyboxInstallDir, File busyboxFile ) throws IOException {
+
+		List<String> args = new ArrayList<>();
+		args.add(busyboxFile.getAbsolutePath());
+
+		args.add("--install");
+		args.add("-s");
+		args.add(".");
+
+		ProcessBuilder processBuilder = new ProcessBuilder()
+			.directory(busyboxInstallDir)
+			.command(args);
+
+		processBuilder.start();
+		processBuilder.redirectError(new File(busyboxInstallDir, "installLog.txt"));
+	}
+
+	public static void writeResource( String resourceName, File outputFile ) throws IOException {
+		outputFile.setReadable(true, false);
+		outputFile.setWritable(true, false);
+		
+		File parentFile = outputFile.getParentFile();
+		if ( !parentFile.exists() ) {
+			parentFile.mkdirs();
+		}
+		if( outputFile.exists()){
+			outputFile.delete();
+		}
+		
+		FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+		InputStream resourceAsStream = Main.class.getResourceAsStream(resourceName);
+		IOUtils.streamTransfer(resourceAsStream, fileOutputStream, true);
+		
+		outputFile.setReadable(true, false);
+		outputFile.setWritable(true, false);
+		outputFile.setExecutable(true, false);
+		
+	}
+
+	/**
+	 * 解压zip
+	 */
+	public static void dealzip( File zipSourcePath, File targetFile ) {
+        //判断目标地址是否存在，如果没有就创建
+		String targetPath = targetFile.getAbsolutePath();
+        File pathFile = new File(targetPath);
+        if ( !pathFile.exists() ) {
+            pathFile.mkdirs();
+        }
+        ZipFile zipFile = null;
+        try {
+            zipFile = new ZipFile(zipSourcePath, Charset.forName("UTF-8"));
+            //若zip中含有中文名文件,换GBK
+            //zip = new ZipFile(zipPath, Charset.forName("GBK"));
+            //遍历里面的文件及文件夹
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while ( entries.hasMoreElements() ) {
+                ZipEntry entry = entries.nextElement();
+				if ( entry.isDirectory() ) {
+					//不解压目录
+					continue;
+				}
+                String zipEntryName = entry.getName();
+				//解压文件
+                InputStream in = zipFile.getInputStream(entry);
+                //也就是把这个文件加入到目标文件夹路径;
+				File outFile = new File(targetFile, zipEntryName.replace('/', File.separatorChar));
+
+                //不存在则创建文件路径
+                File outParentFile = outFile.getParentFile();
+                if ( !outParentFile.exists() ) {
+                    outParentFile.mkdirs();
+                }
+                //文件夹就不解压;
+                if ( outFile.isDirectory() ) {
+					//文件已存在，没有文件则删除文件夹
+					String[] list = outFile.list();
+					if ( list != null && list.length > 0 ) {
+						continue;
+					}
+					outFile.delete();
+                }
+
+                OutputStream out = new FileOutputStream(outFile);
+                byte[] bf = new byte[2048];
+                int len;
+                while ( ( len = in.read(bf) ) > 0 ) {
+                    out.write(bf, 0, len);
+                }
+                in.close();
+                out.close();
+            }
+            zipFile.close();
+        }
+		catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/io/github/zeroaicy/util/IOUtils.java
@@ -0,0 +1,139 @@
+package io.github.zeroaicy.util;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class IOUtils {
+
+	private static final String TAG = "IOUtils";
+
+
+	public static void streamTransfer( InputStream input, OutputStream output ) throws IOException {
+		streamTransfer(input, output, false);
+	}
+
+	public static void streamTransfer( InputStream input, OutputStream output, boolean autoClose ) throws IOException {
+		try {
+			byte[] data = new byte[4096];
+			int read;
+			while ( ( read = input.read(data) ) > 0 ) {
+				output.write(data, 0, read);
+			}
+		}
+		finally {
+			if( autoClose ){
+				close(input);
+				close(output);
+			}
+		}
+	}
+
+	public static byte[] readAllBytes( InputStream inputStream ) throws IOException {
+		return readAllBytes(inputStream, true);
+	}
+
+	public static byte[] readAllBytes( InputStream inputStream, boolean autoClose ) throws IOException {
+		byte[] data = new byte[4096];
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		int count;
+		while ( ( count = inputStream.read(data) ) > 0 ) {
+			baos.write(data, 0, count);
+		}
+		byte[] readAllBytes = baos.toByteArray();
+		baos.close();
+		if ( autoClose ) {
+			inputStream.close();
+		}
+		return readAllBytes;
+	}
+
+	public static void close( AutoCloseable autoCloseable ) {
+		try {
+			if ( autoCloseable != null ) autoCloseable.close();
+		}
+		catch (Throwable e) {}
+	}
+
+	public static List<String> readLines( InputStream input ) {
+		ArrayList<String> lines = new ArrayList<>();
+		readLines(new InputStreamReader(input), lines, true);
+		return lines;
+	}
+	public static void readLines( InputStream input, Collection<String> lines ) {
+		readLines(new InputStreamReader(input), lines, true);
+	}
+
+	public static void readLines( InputStream input, Collection<String> lines, boolean autoClose ) {
+		readLines(new InputStreamReader(input), lines, autoClose);
+	}
+
+	public static void readLines( Reader reader, Collection<String> lines ) {
+		readLines(new BufferedReader(reader), lines, true);
+	}
+
+	public static void readLines( Reader reader, Collection<String> lines, boolean autoClose ) {
+		readLines(new BufferedReader(reader), lines, autoClose);
+	}
+
+	public static void readLines( BufferedReader bufferedReader, Collection<String> lines, boolean autoClose ) {
+		try {
+			String readLine;
+			while ( ( readLine = bufferedReader.readLine() ) != null ) {
+				lines.add(readLine);
+			}
+		}
+		catch (Exception e) {}
+
+		finally {
+			if ( autoClose ) {
+				IOUtils.close(bufferedReader);
+			}
+		}
+	}
+
+	public static void writeLines( Collection<String> lines, String outputPath ) {
+		try {
+			writeLines(lines, new FileOutputStream(outputPath), true);
+		}
+		catch (FileNotFoundException e) {
+			Log.w(TAG, String.format("outputPath: %s 没有发现", outputPath));
+		}
+	}
+	public static void writeLines( Collection<String> lines, File outputFile ) {
+		try {
+			writeLines(lines, new FileOutputStream(outputFile), true);
+		}
+		catch (FileNotFoundException e) {
+			Log.w(TAG, String.format("outputFile: %s 没有发现", outputFile.getAbsolutePath()));
+		}
+	}
+	public static void writeLines( Collection<String> lines, OutputStream output ) {
+		writeLines(lines, output, true);
+	}
+
+	public static void writeLines( Collection<String> lines, OutputStream output, boolean autoClose ) {
+		if ( output == null ) {
+			Log.w(TAG, "output is null");
+		}
+		try {
+			for ( String line : lines ) {
+				output.write(line.getBytes());
+				output.write('\n');
+			}
+		}
+		catch (Throwable e) {}
+		finally {
+			IOUtils.close(output);
+		}
+	}
+}
